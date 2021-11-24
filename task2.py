4ba65ae00df81c81683201e8e7d1216825a826de
Yulia Sheipak <yuliasheip@gmail.com>
2021-11-25 01:31:09 +0300

    task3

--- task2.py
+++ task2.py
@@ -3,9 +3,9 @@ hour = reading // 3600
 minute = (reading % 3600) // 60
 seconds = (reading % 3600) % 60
 if hour < 10:
-     hour =str(0) + str(hour)
+    hour = str(0) + str(hour)
 if minute < 10:
-    minute = str(0)+ str(minute)
-if seconds <10:
-    seconds =str(0)+ str(seconds)
-print(hour,":",minute,":",seconds)
+    minute = str(0) + str(minute)
+if seconds < 10:
+    seconds = str(0) + str(seconds)
+print(hour, ":", minute, ":", seconds)
