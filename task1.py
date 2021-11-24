e9bd57abf37066cb525c3d405e8306917688748b
Yulia Sheipak <yuliasheip@gmail.com>
2021-11-25 00:51:25 +0300

    upd

--- task1.py
+++ task1.py
@@ -6,3 +6,10 @@ length = 1
 print("Добрый день,","я", name)
 print("Мне", age, "год")
 print("Работаю как", occupation, length, "год")
+
+friend_name = input("А как тебя зовут?")
+print(friend_name, ", рада знакомству")
+work_status = input("Ты работаешь?")
+if work_status == "да":
+    print(input("Здорово, а кем?"))
+
